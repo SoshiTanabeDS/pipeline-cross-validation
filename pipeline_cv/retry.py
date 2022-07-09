a58b02545792ed1a8ec8631feb4168a9c742a760
SoshiTanabeDS <tanabe.soshi.y7@daiichisankyo.co.jp>
2022-07-09 22:15:03 +0900

    Add retry.py

---
+++ pipeline_cv/retry.py
@@ -0,0 +1,37 @@
+from sagemaker.workflow.retry import (
+    StepRetryPolicy,
+    StepExceptionTypeEnum,
+    SageMakerJobExceptionTypeEnum,
+    SageMakerJobStepRetryPolicy
+)
+
+
+def retry_policies():
+    retry_policies = [
+        StepRetryPolicy(
+            exception_types=[
+                StepExceptionTypeEnum.SERVICE_FAULT, 
+                StepExceptionTypeEnum.THROTTLING
+            ],
+            expire_after_mins=5,
+            interval_seconds=10,
+            backoff_rate=2.0
+        ),
+        SageMakerJobStepRetryPolicy(
+            exception_types=[SageMakerJobExceptionTypeEnum.RESOURCE_LIMIT],
+            expire_after_mins=120,
+            interval_seconds=60,
+            backoff_rate=2.0
+        ),
+        SageMakerJobStepRetryPolicy(
+            failure_reason_types=[
+                SageMakerJobExceptionTypeEnum.INTERNAL_ERROR,
+                SageMakerJobExceptionTypeEnum.CAPACITY_ERROR,
+            ],
+            max_attempts=10,
+            interval_seconds=30,
+            backoff_rate=2.0
+        )
+    ]
+
+    return retry_policies
