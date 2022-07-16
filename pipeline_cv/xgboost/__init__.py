447eca2286509af84a9aa208b66416f509f67d9e
SoshiTanabeDS <tanabe.soshi.y7@daiichisankyo.co.jp>
2022-07-16 15:46:46 +0900

    Add steps.py

--- pipeline_cv/xgboost/__init__.py
+++ pipeline_cv/xgboost/__init__.py
@@ -2,3 +2,4 @@ from .image import get_xgboost_image_uri
 from .train_step import get_step_train
 from .model_step import get_step_model
 from .transform_step import get_step_transform
+from .steps import create_steps
